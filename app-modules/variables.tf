9c0f458277a8e90152c65f7d546dba14cecfa0a7
christos-tsatsoulas <christos@fastmail.com>
2024-11-07 17:41:31 +0000

    Modules configuration

--- app-modules/variables.tf
+++ app-modules/variables.tf
@@ -3,7 +3,19 @@
 variable "region" {
   description = "Default region for provider"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Name of the web application"
+  type        = string
+  default     = "web-app"
+}
+
+variable "environment_name" {
+  description = "Deployment environment (dev/staging/production)"
+  type        = string
+  default     = "dev"
 }
 
 # EC2 Variables
@@ -11,7 +23,7 @@ variable "region" {
 variable "ami" {
   description = "Amazon machine image to use for ec2 instance"
   type        = string
-  default     = "ami-00385a401487aefa4" # Ubuntu 20.04 LTS // us-east-1
+  default     = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
 }
 
 variable "instance_type" {
@@ -28,6 +40,12 @@ variable "bucket_prefix" {
 }
 
 # Route 53 Variables
+
+variable "create_dns_zone" {
+  description = "If true, create new route53 zone, if false read existing route53 zone"
+  type        = bool
+  default     = false
+}
 
 variable "domain" {
   description = "Domain for website"
